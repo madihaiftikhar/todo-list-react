88f78271b255d88877fb5aa371fc4b9ae890a79e
madihaiftikhar <164077906+madihaiftikhar@users.noreply.github.com>
2024-12-16 11:51:16 -0800

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import TodoApp from "./Components/Todo";
+import "./App.css";
+
+export default function App() {
+    return(
+        <div className="App">
+            <h1>My Todo List</h1>
+            <TodoApp />
+        </div>
+    );
+}
